55628dd7b8c65d03e5ce91e94a114e7a54cbdad7
Gilmar <117695862+Gilmar-PY@users.noreply.github.com>
2024-10-24 09:08:11 -0500

    Update mediator.js

--- proyecto8/src/mediator.js
+++ proyecto8/src/mediator.js
@@ -1,17 +1,25 @@
 import EventEmitter from './event-emitter.js';
 
+//  clase Mediator que centraliza la comunicación entre la aplicación y los plugins.
 class Mediator {
+    // Método para enviar un evento. Llama al método 'emit' del EventEmitter.
     send = (event, data) => {
+        // 'emit' emite el evento especificado y pasa los datos a los observadores suscritos.
         EventEmitter.emit(event, data);
     };
 
+    // Método para suscribirse a un evento. Llama al método 'on' del EventEmitter.
     subscribe = (event, callback) => {
+        // 'on' registra una función (callback) que se ejecutará cuando se emita el evento especificado.
         EventEmitter.on(event, callback);
     };
 
+    // Método  para enviar eventos a los suscriptores (equivalente a 'send').
     sendEventToSubscribers = (event, data) => {
+        // Este método emite el evento a todos los suscriptores registrados, como 'send'.
         EventEmitter.emit(event, data);
     };
 }
 
+// Exporta una única instancia de Mediator para que toda la aplicación use el mismo Mediator compartido.
 export default new Mediator();
